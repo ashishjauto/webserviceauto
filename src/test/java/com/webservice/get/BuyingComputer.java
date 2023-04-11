7c921596acdf30a9dd3caae1a28bbfc4e8407a3e
ashishj <50689398+ashishjauto@users.noreply.github.com>
2023-04-11 07:24:13 -0500

    computer feature
    feature

--- src/test/java/com/webservice/get/BuyingComputer.java
+++ src/test/java/com/webservice/get/BuyingComputer.java
@@ -8,47 +8,47 @@ public class BuyingComputer {
 
     @Given("^I want to buy a PC$")
     public void i_want_to_buy_a_pc() throws Throwable {
-        System.out.println("");
+        System.out.println("I want to buy a PC");
     }
 
     @When("^I select monitor as 15 inch$")
     public void i_select_monitor_as_15_inch() throws Throwable {
-        System.out.println("");
+        System.out.println("I select monitor as 15 inch");
     }
 
     @When("^I select screen as 12 inch$")
     public void i_select_screen_as_12_inch() throws Throwable {
-        System.out.println("");
+        System.out.println("I select screen as 12 inch");
     }
 
     @And("^PC should be of Dell$")
     public void pc_should_be_of_dell() throws Throwable {
-        System.out.println("");
+        System.out.println("PC should be of Dell");
     }
 
     @And("^it should be a desktop$")
     public void it_should_be_a_desktop() throws Throwable {
-        System.out.println("");
+        System.out.println("it should be a desktop");
     }
 
     @And("^a 'carbon' finish$")
     public void a_carbon_finish() throws Throwable {
-        System.out.println("");
+        System.out.println("a 'carbon' finish");
     }
 
     @And("^it should be a laptop$")
     public void it_should_be_a_laptop() throws Throwable {
-        System.out.println("");
+        System.out.println("it should be a laptop");
     }
 
     @And("^a 'sleak silver' finish$")
     public void a_sleak_silver_finish() throws Throwable {
-        System.out.println("");
+        System.out.println("a 'sleak silver' finish");
     }
 
     @And("^I select hard drive 1 TB$")
     public void i_select_hard_drive_1_tb() throws Throwable {
-        System.out.println("");
+        System.out.println("I select hard drive 1 TB");
     }
 
 }
--- src/test/resources/computer.feature
+++ src/test/resources/computer.feature
@@ -1,20 +1,19 @@
-@Buycomputer
-  Feature: Buying a computer
-    I want to use this tempalte for my feature file
+Feature: Buying a computer
+  I want to use this template for my feature file
   Background: Buying Dell PC
     Given I want to buy a PC
     And PC should be of Dell
 
-    @BuyingDesktop
-    Scenario: Buying a desktop
-      And it should be a desktop
-      When I select monitor as 15 inch
-      And a 'carbon' finish
+  @BuyingDesktop
+  Scenario: Buying a desktop
+    And it should be a desktop
+    When I select monitor as 15 inch
+    And a 'carbon' finish
 
+  @BuyingTab
+  Scenario: Buying a laptop
+    And it should be a laptop
+    When I select screen as 12 inch
+    And a 'sleak silver' finish
+    And I select hard drive 1 TB
 
-      @BuyingTab
-      Scenario: Buying a laptop
-        And it should be a laptop
-        When I select screen as 12 inch
-        And a 'sleak silver' finish
-        And I select hard drive 1 TB
